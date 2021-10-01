67dde12761bc7802a5f023d96266b107b3c2a0ca
juanlarenas <juan.larenas@alumnos.uv.cl>
2021-10-01 13:18:40 +0000

    update script.sh

--- src/script.sh
+++ src/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "nombre del archivo es: " $0
 echo  "Parametros ingresados: " $@
-sleep 5 &
+sleep 5 & 
+#$!: guarda el pid del ultimo proceso en estaco el sleep
 process_id=$!
 echo "PID: $process_id"
 kill -SIGSTOP $process_id
